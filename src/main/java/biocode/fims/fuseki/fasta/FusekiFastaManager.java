1f664b2860553c9d89c77e3896582a4e91eb7dde
rjewing <ewing.rj@gmail.com>
2016-07-08 06:52:45 -0700

    fix bug when project doesn't contain SEQUENCE_URI

--- src/main/java/biocode/fims/fuseki/fasta/FusekiFastaManager.java
+++ src/main/java/biocode/fims/fuseki/fasta/FusekiFastaManager.java
@@ -79,16 +79,19 @@ public class FusekiFastaManager extends FastaManager {
     @Override
     public void copySequences(String previousGraph, String newGraph) {
         Entity rootEntity = FastaUtils.getEntityRoot(processController.getMapping(), FastaSequence.SEQUENCE_URI);
-        String insert = "INSERT { GRAPH <" + newGraph + "> { ?s <" + FastaSequence.SEQUENCE_URI + "> ?o }} WHERE " +
-                "{ GRAPH <" + newGraph + "> { ?s a <" + rootEntity.getConceptURI() + "> } . " +
-                "GRAPH <" + previousGraph + "> { ?s <" + FastaSequence.SEQUENCE_URI + "> ?o }}";
+        // if rootEntity is null, then there is no SEQUENCE_URI for this project
+        if (rootEntity != null) {
+            String insert = "INSERT { GRAPH <" + newGraph + "> { ?s <" + FastaSequence.SEQUENCE_URI + "> ?o }} WHERE " +
+                    "{ GRAPH <" + newGraph + "> { ?s a <" + rootEntity.getConceptURI() + "> } . " +
+                    "GRAPH <" + previousGraph + "> { ?s <" + FastaSequence.SEQUENCE_URI + "> ?o }}";
 
-        UpdateRequest update = UpdateFactory.create(insert);
+            UpdateRequest update = UpdateFactory.create(insert);
 
-        UpdateProcessRemote riStore = (UpdateProcessRemote)
-                UpdateExecutionFactory.createRemote(update, fusekiService + "/update");
+            UpdateProcessRemote riStore = (UpdateProcessRemote)
+                    UpdateExecutionFactory.createRemote(update, fusekiService + "/update");
 
-        riStore.execute();
+            riStore.execute();
+        }
     }
 
     /**
@@ -101,6 +104,10 @@ public class FusekiFastaManager extends FastaManager {
         ArrayList<String> datasetIds = new ArrayList<>();
         String graph = fetchGraph();
         Entity rootEntity = FastaUtils.getEntityRoot(processController.getMapping(), FastaSequence.SEQUENCE_URI);
+
+        if (rootEntity == null) {
+            throw new ServerErrorException("Server Error", "No entity was found containing a urn:sequence attribute");
+        }
 
         if (graph != null) {
             // query fuseki graph
@@ -128,6 +135,10 @@ public class FusekiFastaManager extends FastaManager {
 
     private String getEntityRootIdentifier() {
         Entity rootEntity = FastaUtils.getEntityRoot(processController.getMapping(), FastaSequence.SEQUENCE_URI);
+
+        if (rootEntity == null) {
+            throw new ServerErrorException("Server Error", "No entity was found containing a urn:sequence attribute");
+        }
 
         // get the bcidRoot so we can parse the identifier from the fuseki db
         Bcid bcid = expeditionService.getEntityBcid(
