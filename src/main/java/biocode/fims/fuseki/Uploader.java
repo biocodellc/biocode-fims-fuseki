293640b7fcff85f70a80a39efdfce57b72cd52d9
John Deck <jdeck88@gmail.com>
2017-04-24 20:37:56 -0700

    updating jena

--- src/main/java/biocode/fims/fuseki/Uploader.java
+++ src/main/java/biocode/fims/fuseki/Uploader.java
@@ -5,7 +5,8 @@ import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.ReadWrite;
 import com.hp.hpl.jena.tdb.TDBFactory;
 import com.hp.hpl.jena.update.*;
-import org.openjena.atlas.lib.StrUtils;
+
+import org.apache.jena.atlas.lib.StrUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
